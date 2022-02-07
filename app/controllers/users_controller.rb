eb2d6d3ad514fb1a53d2fe6ff735e833454a3308
Ika-Taro <black.jkh@gmail.com>
2022-02-07 16:39:53 +0900

    画面遷移の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,10 @@ class UsersController < ApplicationController
     else
       render :edit
     end
+  end private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 
 end
